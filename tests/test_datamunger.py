a2635211004909748c765fb096c879550d95e8f5
Rushil-Daya <1830490@students.wits.ac.za>
2020-06-08 11:45:49 +0200

    first testcase done

--- tests/test_datamunger.py
+++ tests/test_datamunger.py
@@ -1,6 +1,15 @@
 import unittest
+import datamunger
 
 class TestDatamunger(unittest.TestCase) :
+
+    def test_calcTotal(self):
+        arrayTemp= [10,9,8,7,6,5,4,3,2]
+        total=44
+        cal = datamunger.calc_total(arrayTemp)
+        self.assertEqual(cal,total)
+
+    
 
 
 if __name__ == "__main__":
