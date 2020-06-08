059d0803a0fedfcbcf45567bd12fe91798394018
Rushil-Daya <1830490@students.wits.ac.za>
2020-06-08 12:10:34 +0200

    added second test case

--- tests/test_datamunger.py
+++ tests/test_datamunger.py
@@ -4,10 +4,14 @@ import datamunger
 class TestDatamunger(unittest.TestCase) :
 
     def test_calcTotal(self):
-        arrayTemp= [10,9,8,7,6,5,4,3,2]
-        total=44
+        arrayTemp= [10,9,8,7,6,5,4,3,2]     #create a temporary array which represents a link from the .csv file
+        total=44                               #this is the correct total (adding T1 throught T8)
         cal = datamunger.calc_total(arrayTemp)
         self.assertEqual(cal,total)
+
+    def test_checkRow(self):
+        arrTemp= [10,9,7,6,4,3,2]
+        self.assertEqual(datamunger.check_row(1,[],arrTemp), False)
 
     
 
